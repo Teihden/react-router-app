b0501de6a24a068271e5300a5b8f90f873d7883f
Denis Teikhrib <tdv@d-element.ru>
2025-06-21 20:54:05 +0500

    fix: minor fixes

--- app/root.tsx
+++ app/root.tsx
@@ -71,16 +71,16 @@ export default function App({ loaderData }: Route.ComponentProps) {
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="stylesheet" href={appStylesHref}/>
-    </head>
-    <body>
-    {children}
-    <ScrollRestoration/>
-    <Scripts/>
-    </body>
+      <head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href={appStylesHref}/>
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration/>
+        <Scripts/>
+      </body>
     </html>
   );
 }
