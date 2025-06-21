1689219297efcd96e2c2a7088c8fa5e25add4717
Denis Teikhrib <tdv@d-element.ru>
2025-06-21 23:00:03 +0500

    feat: add HydrateFallback

--- app/root.tsx
+++ app/root.tsx
@@ -75,6 +75,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" href={appStylesHref}/>
+        <title>Address Book</title>
       </head>
       <body>
         {children}
@@ -113,5 +114,14 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         </pre>
       )}
     </main>
+  );
+}
+
+export function HydrateFallback() {
+  return (
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
   );
 }
