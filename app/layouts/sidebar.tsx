2d6ef18540dd2ede07b759bf01e107014476b2fb
Denis Teikhrib <tdv@d-element.ru>
2025-06-23 16:17:39 +0500

    fix: sidebar.tsx

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -35,7 +35,12 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
           <Form
             id="search-form"
             role="search"
-            onChange={(e) => submit(e.currentTarget)}
+            onChange={(e) => {
+              const isFirstSearch = q === null;
+              submit(e.currentTarget, {
+                replace: !isFirstSearch,
+              });
+            }}
           >
             <input
               id="q"
